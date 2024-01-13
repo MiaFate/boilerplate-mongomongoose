0ec8a8166022580173cda53ddf6c5292ebdcb3fe
miafate <miapurplefroot@gmail.com>
2024-01-13 03:42:28 -0300

    use model.find to search your database, fix

--- myApp.js
+++ myApp.js
@@ -36,7 +36,7 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-  Person.find(personName, function(error, personFound) {
+  Person.find({ name: personName }, function(error, personFound) {
     if (error) return console.log(error)
     done(null, personFound);
   })
